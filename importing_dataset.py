33db1e8cb13d2a7fcd02174e1173c9fb3666ea4d
goyalanu <33743053+goyalanu@users.noreply.github.com>
2019-07-22 15:39:55 +0530

    Create importing_dataset.py

---
+++ importing_dataset.py
@@ -0,0 +1,8 @@
+#Importing Dataset
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
